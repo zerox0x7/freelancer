43cd5c4564dae0783b45d8620df8d9ba5908159e
zerox0x7 <rfdc804@gmail.com>
2025-04-26 19:07:23 +0300

    second update

--- resources/views/vendor/Chatify/layouts/listItem.blade.php
+++ resources/views/vendor/Chatify/layouts/listItem.blade.php
@@ -1,32 +1,13 @@
-{{-- -------------------- Saved Messages -------------------- --}}
-@if($get == 'saved')
-    <!-- <table class="messenger-list-item" data-contact="{{ Auth::user()->id }}">
-        <tr data-action="0">
-            {{-- Avatar side --}}
-            <td>
-            <div class="saved-messages avatar av-m">
-                <span class="far fa-bookmark"></span>
-            </div>
-            </td>
-            {{-- center side --}}
-            <td>
-                <p data-id="{{ Auth::user()->id }}" data-type="user">الرسائل المحفوظة <span>أنت</span></p>
-                <span>احفظ رسائلك بشكل آمن</span>
-            </td>
-        </tr>
-    </table> -->
-@endif
-
 {{-- -------------------- Contact list -------------------- --}}
 @if($get == 'users' && !!$lastMessage)
 <?php
 $lastMessageBody = mb_convert_encoding($lastMessage->body, 'UTF-8', 'UTF-8');
 $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0, 30, 'UTF-8').'..' : $lastMessageBody;
 ?>
-<table class="messenger-list-item" data-contact="{{ $user->id }}">
+<table class="messenger-list-item" data-contact="{{ $user->id }}" style="padding: 10px 5px; border-radius: 5px;">
     <tr data-action="0">
         {{-- Avatar side --}}
-        <td style="position: relative">
+        <td style="position: relative; padding-right: 12px; vertical-align: middle; width: 60px;">
             @if($user->active_status)
                 <span class="activeStatus active"></span>
             @endif
@@ -38,11 +19,11 @@ $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0
         </div>
         </td>
         {{-- center side --}}
-        <td>
-        <p data-id="{{ $user->id }}" data-type="user">
+        <td style="padding-left: 8px; vertical-align: middle;">
+        <p data-id="{{ $user->id }}" data-type="user" style="margin: 0 0 5px 0; display: flex; justify-content: space-between;">
             {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
-            <span class="contact-item-time" data-time="{{$lastMessage->created_at}}">{{ $lastMessage->timeAgo }}</span></p>
-        <span>
+            <span style="color: #E7CB73 !important;" class="contact-item-time" data-time="{{$lastMessage->created_at}}">{{ $lastMessage->timeAgo }}</span></p>
+        <span style="display: block; max-width: 100%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
             {{-- Last Message user indicator --}}
             {!!
                 $lastMessage->from_id == Auth::user()->id
@@ -59,7 +40,7 @@ $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0
             @endif
         </span>
         {{-- New messages counter --}}
-            {!! $unseenCounter > 0 ? "<b>".$unseenCounter."</b>" : '' !!}
+            {!! $unseenCounter > 0 ? "<b style='float: right; margin-top: 5px; background-color: var(--primary-color); padding: 0 5px; border-radius: 10px; font-size: 12px; color: white;'>".$unseenCounter."</b>" : '' !!}
         </td>
     </tr>
 </table>
@@ -67,10 +48,10 @@ $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0
 
 {{-- -------------------- Search Item -------------------- --}}
 @if($get == 'search_item')
-<table class="messenger-list-item" data-contact="{{ $user->id }}">
+<table class="messenger-list-item" data-contact="{{ $user->id }}" style="padding: 8px 5px; border-radius: 5px;">
     <tr data-action="0">
         {{-- Avatar side --}}
-        <td>
+        <td style="padding-right: 12px; vertical-align: middle; width: 60px;">
         <div class="avatar av-m"
         style="background-image: url('{{ $user->avatar }}');">
             @if(!$user->avatar)
@@ -79,8 +60,8 @@ $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0
         </div>
         </td>
         {{-- center side --}}
-        <td>
-            <p data-id="{{ $user->id }}" data-type="user">
+        <td style="padding-left: 8px; vertical-align: middle;">
+            <p data-id="{{ $user->id }}" data-type="user" style="margin: 0;">
             {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }}
         </td>
 
@@ -90,7 +71,18 @@ $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0
 
 {{-- -------------------- Shared photos Item -------------------- --}}
 @if($get == 'sharedPhoto')
-<div class="shared-photo chat-image" style="background-image: url('{{ $image }}')"></div>
+<div class="shared-photo chat-image" style="background-image: url('{{ $image }}'); border-radius: 8px;"></div>
 @endif
+
+<style>
+.messenger-list-item:hover {
+    background-color: rgba(0,0,0,0.04);
+    transition: background-color 0.3s;
+}
+.activeStatus {
+    bottom: 0px;
+    right: 5px;
+}
+</style>
 
 
